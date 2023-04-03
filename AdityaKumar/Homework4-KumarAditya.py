1f95d12a5cfc7618948be8f065261556bb0baf71
Aditya kumar <yomecool@gmail.com>
2023-04-03 00:36:13 -0400

    Add-on

--- AdityaKumar/Homework4-KumarAditya.py
+++ AdityaKumar/Homework4-KumarAditya.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-from toolbox import Cal_rolling_mean_var, cal_autocorr
+from toolbox import cal_autocorr
+from tabulate import tabulate
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -13,6 +14,49 @@ mean_e = 2
 variance_e = 1
 e1 = np.random.normal(mean_e, variance_e, N)
 
-# Q1.
-# b)
+# Q1: AR(2)
+# 𝑦(𝑡)−0.5𝑦(𝑡−1)−0.2𝑦(𝑡−2)=𝑒(𝑡)
+# Q1 b)
+num = [1,0,0]
+den = [1,-0.5,-0.2]
+system = (num, den, 1)
+t, y1 = signal.dlsim(system, e1)
 
+# Q1 c)
+print(f"For 100 Samples:")
+print(f"The Experimental Mean of y(t) - 0.5 y(t-1) - 0.2y(t-2) = e(t) is:{np.mean(y1):.4f}")
+print(f"The Experimental Variance of y(t) - 0.5 y(t-1) - 0.2y(t-2) = e(t) is:{np.var(y1):.4f}\n")
+
+# Q2 d)
+np.random.seed(6313)
+N1 = 1000
+e2 = np.random.normal(mean_e, variance_e, N1)
+t, y2 = signal.dlsim(system, e2)
+print(f"For 1000 Samples:")
+print(f"The Experimental Mean of y(t) - 0.5 y(t-1) - 0.2y(t-2) = e(t) is:{np.mean(y2):.4f}")
+print(f"The Experimental Variance of y(t) - 0.5 y(t-1) - 0.2y(t-2) = e(t) is:{np.var(y2):.4f}\n")
+
+np.random.seed(6313)
+N2 = 10000
+e3 = np.random.normal(mean_e, variance_e, N2)
+t, y3 = signal.dlsim(system, e3)
+print(f"For 10000 Samples:")
+print(f"The Experimental Mean of y(t) - 0.5 y(t-1) - 0.2y(t-2) = e(t) is:{np.mean(y3):.4f}")
+print(f"The Experimental Variance of y(t) - 0.5 y(t-1) - 0.2y(t-2) = e(t) is:{np.var(y3):.4f}\n")
+
+# Q1 e)
+
+table = [["Number of Samples","Theoretical Mean","Experimental Mean","Theoretical Variance","Experimental  Variance"],["100","6.6667",round(np.mean(y1),4),"1.7094",round(np.var(y1),4)],["1000","6.6667",round(np.mean(y2),4),"1.7094",round(np.var(y2),4)],["10,000","6.6667",round(np.mean(y3),4),"1.7094",round(np.var(y3),4)]]
+
+print(tabulate(table,headers='firstrow', tablefmt = 'fancy_grid'))
+
+# Q1 f)
+lag1 = 20
+lag2 = 40
+lag3 = 80
+cal_autocorr(y1,lag1,"AR(2)")
+plt.show()
+cal_autocorr(y1,lag2,"AR(2)")
+plt.show()
+cal_autocorr(y1,lag3,"AR(2)")
+plt.show()
