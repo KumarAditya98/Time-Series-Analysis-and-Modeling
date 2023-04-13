a016c192d1f738dd8a149f15de3aff9ca4340a39
Aditya kumar <yomecool@gmail.com>
2023-04-13 16:08:06 -0400

    Add-on

--- AdityaKumar/toolbox.py
+++ AdityaKumar/toolbox.py
@@ -129,11 +129,10 @@ def ARMA_process():
     mean_e = int(input("Enter the mean of white noise:"))
     var_e = int(input("Enter the variance of white noise:"))
     ar_order = int(input("Enter the AR portion order:"))
-    ma_order = int(input("Enter the MA portion order:\n"))
+    ma_order = int(input("Enter the MA portion order:"))
     if ar_order == 0 and ma_order == 0:
         print("This is just a white noise. Run program again.")
         return None
-    print("\n")
     print('Enter the respective coefficients in the form:\n[y(t) + a1*y(t-1)) + a2*y(t-2) + ... = e (t) + b1*e(t-1) + b2*e(t-2) + ...]\n')
     ar_coeff = []
     for i in range(ar_order):
@@ -150,10 +149,10 @@ def ARMA_process():
     y = arma_process.generate_sample(N, scale=np.sqrt(var_e)) + mean_y
     lags = 60
     if arma_process.isstationary:
-        print('Process with given coefficeints is Stationary.')
+        print('Process with given coefficients is Stationary.')
         ry = arma_process.acf(lags=lags)
     else:
-        print('Process with given coefficeints is Non-Stationary.')
+        print('Process with given coefficients is Non-Stationary.')
         ry = sm.tsa.stattools.acf(y, nlags=lags)
     ryy = ry[::-1]
     Ry = np.concatenate((ryy, ry[1:]))
@@ -178,19 +177,11 @@ def Cal_GPAC(ry,j=7,k=7):
                 Denn = den[:,::-1]
                 den_det = np.linalg.det(Denn)
                 num = Denn.copy()
-                # num = np.empty((i+1,i+1))
-                # for b in range(1,i+1):
-                #     temp = []
-                #     for a in range(mid_point - i +b +l,mid_point+b+l+1):
-                #         temp.append(ry[a])
-                #     temp1 = np.array(temp)
-                #     num[:,b] = temp1
                 temp = []
                 for c in range(i+1):
                     temp.append(ry[mid_point+l+1+c])
                 temp1 = np.array(temp)
                 num[:,-1] = temp1
-                # Numm = num[:, ::-1]
                 num_det = np.linalg.det(num)
                 if den_det < 0.0001:
                     col.append(np.inf)
@@ -200,7 +191,7 @@ def Cal_GPAC(ry,j=7,k=7):
         matrix[:,i] = col
     np.set_printoptions(precision=3,floatmode='fixed')
     fig, ax = plt.subplots(figsize = (12,8))
-    sns.heatmap(matrix,annot=True,cmap='coolwarm',ax=ax,fmt='.3f',xticklabels=list(range(1,k+1)),yticklabels=list(range(j)),annot_kws={"size": 30 / np.sqrt(len(matrix)),"fontweight":'bold'})
+    sns.heatmap(matrix,annot=True,cmap='coolwarm',ax=ax,fmt='.3f',xticklabels=list(range(1,k+1)),yticklabels=list(range(j)),annot_kws={"size": 30 / np.sqrt(len(matrix)),"fontweight":'bold'},vmin=np.min(matrix),vmax=np.max(matrix))
     ax.tick_params(labelsize=30 / np.sqrt(len(matrix)))
     cbar = ax.collections[0].colorbar
     cbar.ax.tick_params(labelsize=30 / np.sqrt(len(matrix)),width=2)
